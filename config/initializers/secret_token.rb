7eccd3cb3e3eb7ac45d7f603a91aae59b384619d
Christian Roman <chroman16@gmail.com>
2013-05-09 21:27:27 -0500

    First commit with devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tuhmorelos::Application.config.secret_token = '5545cc1230076072f7555d6584e20126f9950e09c12fd8656fc5aa38a6ff47735afae993d7f39411619f482bd649bcded193cb88be05376756c7933f555d2ed4'
