0512ce69d7ee0141810887711cebd9e8aff02022
Christian Roman <chroman16@gmail.com>
2013-05-14 23:06:41 -0500

    Checkboxes all

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-    //alert(2);
+
+    $('.check_all').change(function(){
+	var divParent = $(this).parent('div');
+	var checkboxes = divParent.find('input:checkbox');
+
+	if( $(this).is(':checked') ) 
+	    checkboxes.each(function(){
+		$(this).prop({checked: true});
+	    });
+	else 
+	    checkboxes.each(function(){
+		$(this).prop({checked: false});
+	    });
+    }); 
+
 });
