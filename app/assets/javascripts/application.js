fd4b128cfb2230493ffd3bdeb2358be1da4b2cc0
Christian Roman <chroman16@gmail.com>
2013-06-15 00:17:54 -0500

    assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,13 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require_tree .
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-datepicker
+//= require zoom-showcase
+//= require jquery.easing
+//= require jquery.clip-animation
+//= require jquery.fancybox
 
 $(function(){
 
@@ -19,22 +25,22 @@ $(function(){
 
     if(arr.length){
 
-	(function recurse(counter) {
-	    var color = arr[counter];
+    (function recurse(counter) {
+        var color = arr[counter];
 
-	    $('html').css('background', 'white url('+ color +') no-repeat center center fixed')
+	$('html').css('background', 'white url('+ color +') no-repeat center center fixed')
 	    .css('-webkit-background-size', 'cover')
 	    .css('-moz-background-size', 'cover')
 	    .css('-o-background-size', 'cover')
 	    .css('background-size', 'cover')
 
-	    delete arr[counter];
-	arr.push(color);
-	setTimeout(function() {
+        delete arr[counter];
+        arr.push(color);
+        setTimeout(function() {
 	    recurse(counter + 1);
 	}, 8000);
-	})(0);
+    })(0);
 
     }
-
+    
 });
