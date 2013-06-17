f8468677e31a32daf570a37740b9365c8d2fc4f3
Christian Roman <chroman16@gmail.com>
2013-06-17 14:35:30 -0500

    Background fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,30 +5,15 @@
 //= require jquery.easing
 //= require jquery.clip-animation
 //= require jquery.fancybox
+//= require jquery.backstretch
+
 
 $(function(){
 
     var arr = [];
     $('.background-images span').each(function(){ arr.push( $(this).data('url') ) });
+ 
+    $.backstretch(arr, {duration: 3000, fade: 750});
 
-    if(arr.length){
-
-    (function recurse(counter) {
-        var color = arr[counter];
-
-	$('html').css('background', 'white url('+ color +') no-repeat center center fixed')
-	    .css('-webkit-background-size', 'cover')
-	    .css('-moz-background-size', 'cover')
-	    .css('-o-background-size', 'cover')
-	    .css('background-size', 'cover')
-
-        delete arr[counter];
-        arr.push(color);
-        setTimeout(function() {
-	    recurse(counter + 1);
-	}, 8000);
-    })(0);
-
-    }
-    
 });
+
