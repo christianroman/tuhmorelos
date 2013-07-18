38b279ea9fee057d827926e17710e8782b5bfee0
Christian Roman <chroman16@gmail.com>
2013-07-18 00:21:28 -0500

    API fix

--- app/controllers/api/hotels_controller.rb
+++ app/controllers/api/hotels_controller.rb
@@ -2,7 +2,7 @@ module Api
     class HotelsController < ApplicationController
 
 	def index
-	    @hotels = Hotel.search(params[:destinations], params[:fares], params[:categories])
+	    @hotels = Hotel.search(params[:destinations], params[:fares], params[:categories], params[:promos])
 	    @status = @hotels.blank? ? "NO_RESULTS" : "OK"
 	    self.response.headers["Content-Type"] = "application/json; charset=UTF-8"
 	end
